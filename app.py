235f24b4a41bb657dc4a2ef1bc0d14ddac668243
fcebk17 <fcebk17@gmail.com>
2022-07-25 01:04:38 +0800

    Revise to correct port

--- app.py
+++ app.py
@@ -10,8 +10,49 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """index page."""
+    return render_template('index.html')
+
+@app.route('/demo.html')
+def test():
     """Video streaming home page."""
     return render_template('demo.html')
+
+@app.route('/bodySelect.html')
+def bodymode():
+    """Select body mode page."""
+    return render_template('bodySelect.html')
+
+@app.route('/foot_levelmap.html')
+def foot_levelmap():
+    """ Select Foot Level map page."""
+    return render_template('foot_levelmap.html')
+
+@app.route('/gamemode.html')
+def gamemode():
+    """ Select gamemode page."""
+    return render_template('gamemode.html')
+
+@app.route('/hands_levelmap.html')
+def hands_levelmap():
+    """Select Hands Level map page."""
+    return render_template('hands_levelmap.html')
+
+@app.route('/head_levelmap.html')
+def head_levelmap():
+    """ Select Head Level map page."""
+    return render_template('head_levelmap.html')
+
+@app.route('/levelmap.html')
+def levelmap():
+    """ Select level map page."""
+    return render_template('levelmap.html')
+
+@app.route('/master.html')
+def master_mode():
+    """ Select Master mode page."""
+    return render_template('master.html')
+
 
 
 def gen(camera):
