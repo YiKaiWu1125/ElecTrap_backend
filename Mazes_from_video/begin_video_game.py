64677fe6f28cdcadf5dfe1020a2fcea5032867e3
asas1asas200 <asas1asas200@gmail.com>
2022-07-29 03:04:27 +0800

    restyle: format the code by black

--- Mazes_from_video/begin_video_game.py
+++ Mazes_from_video/begin_video_game.py
@@ -2,6 +2,7 @@ from os import lstat
 from textwrap import indent
 import cv2
 import mediapipe as mp
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
@@ -11,92 +12,138 @@ import numpy
 import playvideo_and_getcoordinate as p_g
 
 
-def draw(image,right_hand,binarization_arr,nownumber,half_track_width):
-    
-    for i in range(1,len(right_hand)):
-        image=p_g.draw_and_save_circle_link_poly(image,right_hand[i-1],right_hand[i],i,binarization_arr,0,half_track_width)
-    for i in range (len(right_hand)):
-        image = p_g.draw_circle(image,right_hand,nownumber,half_track_width)
+def draw(image, right_hand, binarization_arr, nownumber, half_track_width):
+
+    for i in range(1, len(right_hand)):
+        image = p_g.draw_and_save_circle_link_poly(
+            image,
+            right_hand[i - 1],
+            right_hand[i],
+            i,
+            binarization_arr,
+            0,
+            half_track_width,
+        )
+    for i in range(len(right_hand)):
+        image = p_g.draw_circle(image, right_hand, nownumber, half_track_width)
     return image
 
-def judgment_status(image,right_hand,binarization_arr,x,y,now_number,sta,game_begin_time,game_end_time,half_track_width):
-    front = (now_number-5)
-    end = (now_number+5)
-    if front < 0 :
+
+def judgment_status(
+    image,
+    right_hand,
+    binarization_arr,
+    x,
+    y,
+    now_number,
+    sta,
+    game_begin_time,
+    game_end_time,
+    half_track_width,
+):
+    front = now_number - 5
+    end = now_number + 5
+    if front < 0:
         front = 0
     if end >= len(binarization_arr):
-        end = len(binarization_arr)-1
-    if now_number == len(binarization_arr)-1 : 
-        sta = 'game_over'
-        game_end_time = time.time() 
-    if now_number == 0 :
+        end = len(binarization_arr) - 1
+    if now_number == len(binarization_arr) - 1:
+        sta = "game_over"
+        game_end_time = time.time()
+    if now_number == 0:
         if binarization_arr[0][x][y] == 1:
             now_number = 1
-            sta = 'playing'
-            game_begin_time = time.time() 
-    else :
+            sta = "playing"
+            game_begin_time = time.time()
+    else:
         k = True
-        for i in range (end,front-1,-1):
-            if binarization_arr[i][x][y] == 1 :
+        for i in range(end, front - 1, -1):
+            if binarization_arr[i][x][y] == 1:
                 now_number = i
                 k = False
                 break
         if k == True:
             now_number = 0
-            sta = 'prepare_begin'
+            sta = "prepare_begin"
 
-    if sta != 'game_over':
-        image = draw(image,right_hand,binarization_arr,now_number,half_track_width)   # 畫出電管
-    if sta == 'prepare_begin':
-        image = cv2.circle(image, (right_hand[0][0],right_hand[0][1]), half_track_width, (255,0,0), -1)# 畫出起始位置框
-    if sta == 'playing':
-        cv2.circle(image, (right_hand[len(right_hand)-1][0],right_hand[len(right_hand)-1][1]), half_track_width, (255,0,0), -1)   # 畫出終止位置框
-    if sta == 'game_over':
-        cv2.putText(image, "score:" + str(int(game_end_time - game_begin_time)) + " s.",
-                    (0, 150), cv2.FONT_HERSHEY_PLAIN, 5, (260, 25, 240), 3)  # 檢視成績
-        cv2.putText(image, "<Game over>", (0, 250),
-                    cv2.FONT_HERSHEY_PLAIN, 5, (260, 25, 240), 3)
-        #cv2.rectangle(image, (155, 280), (420, 330),
+    if sta != "game_over":
+        image = draw(
+            image, right_hand, binarization_arr, now_number, half_track_width
+        )  # 畫出電管
+    if sta == "prepare_begin":
+        image = cv2.circle(
+            image,
+            (right_hand[0][0], right_hand[0][1]),
+            half_track_width,
+            (255, 0, 0),
+            -1,
+        )  # 畫出起始位置框
+    if sta == "playing":
+        cv2.circle(
+            image,
+            (right_hand[len(right_hand) - 1][0], right_hand[len(right_hand) - 1][1]),
+            half_track_width,
+            (255, 0, 0),
+            -1,
+        )  # 畫出終止位置框
+    if sta == "game_over":
+        cv2.putText(
+            image,
+            "score:" + str(int(game_end_time - game_begin_time)) + " s.",
+            (0, 150),
+            cv2.FONT_HERSHEY_PLAIN,
+            5,
+            (260, 25, 240),
+            3,
+        )  # 檢視成績
+        cv2.putText(
+            image, "<Game over>", (0, 250), cv2.FONT_HERSHEY_PLAIN, 5, (260, 25, 240), 3
+        )
+        # cv2.rectangle(image, (155, 280), (420, 330),
         #              (0, 25, 240), 5)   # 再玩一次
-        #cv2.putText(image, "play again", (160, 320),
+        # cv2.putText(image, "play again", (160, 320),
         #            cv2.FONT_HERSHEY_PLAIN, 3, (0, 25, 240), 3)  # 再玩一次
-        #cv2.rectangle(image, (155, 350), (420, 400),
+        # cv2.rectangle(image, (155, 350), (420, 400),
         #              (0, 25, 240), 5)   # 遊戲結束
-        #cv2.putText(image, "end game", (160, 390),
+        # cv2.putText(image, "end game", (160, 390),
         #            cv2.FONT_HERSHEY_PLAIN, 3, (0, 25, 240), 3)  # 遊戲結束
-    #print("sta is :"+str(sta))
-    return image,now_number,sta,game_begin_time,game_end_time
+    # print("sta is :"+str(sta))
+    return image, now_number, sta, game_begin_time, game_end_time
 
-def begin_video_game(width,hight,half_track_width,sample_video,video_id):
 
-    right_hand,binarization_arr=p_g.get_track(sample_video,width,hight,half_track_width)
+def begin_video_game(width, hight, half_track_width, sample_video, video_id):
+
+    right_hand, binarization_arr = p_g.get_track(
+        sample_video, width, hight, half_track_width
+    )
     print("successful get coordinate.")
 
     # For webcam input:
     cap = cv2.VideoCapture(video_id)
-    #挑整與顯示畫質
+    # 挑整與顯示畫質
     wi = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     hi = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     print("webcam init Image Size: %d x %d" % (wi, hi))
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width) #設定解析度
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, hight) #設定解析度
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)  # 設定解析度
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, hight)  # 設定解析度
     wi = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     hi = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     print("webcam fix Image Size: %d x %d" % (wi, hi))
 
-    sta = 'prepare_begin'
+    sta = "prepare_begin"
     now_number = 0
-    game_end_time = 0 
-    game_begin_time = 0 
+    game_end_time = 0
+    game_begin_time = 0
     x = y = 0
 
-
-    with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
+    with mp_pose.Pose(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as pose:
         while cap.isOpened():
             success, image = cap.read()
             if not success:
                 ##can use to debug map
-                #for i in range(len(binarization_arr)):
+                # for i in range(len(binarization_arr)):
                 #    rmg = img
                 #    for h in range (hight+5):
                 #        for w in range (width+5):
@@ -104,8 +151,8 @@ def begin_video_game(width,hight,half_track_width,sample_video,video_id):
                 #                rmg=cv2.rectangle(rmg,(w-1,h-1),(w+1,h+1),(255,150,200),8)   # 畫出手的位置
                 #    cv2.imshow('show', rmg)
                 #    if cv2.waitKey(5) & 0xFF == 27:
-                #        break                          
-                #time.sleep(5)
+                #        break
+                # time.sleep(5)
 
                 print("Ignoring empty camera frame.")
                 # If loading a video, use 'break' instead of 'continue'.
@@ -118,21 +165,21 @@ def begin_video_game(width,hight,half_track_width,sample_video,video_id):
             image.flags.writeable = False
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             img = image
-            img=cv2.flip(img, 1) # 畫面翻轉
-            #img = cv2.resize(image,(width,hight))  # 調整畫面尺寸
+            img = cv2.flip(img, 1)  # 畫面翻轉
+            # img = cv2.resize(image,(width,hight))  # 調整畫面尺寸
             results = pose.process(img)
 
-            size = img.shape   # 取得攝影機影像尺寸
-            w = size[1]        # 取得畫面寬度
-            h = size[0]        # 取得畫面高度
+            size = img.shape  # 取得攝影機影像尺寸
+            w = size[1]  # 取得畫面寬度
+            h = size[0]  # 取得畫面高度
 
             if results.pose_landmarks:
                 rx = p_g.zero_to_one(results.pose_landmarks.landmark[20].x)
                 ry = p_g.zero_to_one(results.pose_landmarks.landmark[20].y)
-                x = int(rx *w)
-                y = int(ry *h)
+                x = int(rx * w)
+                y = int(ry * h)
             else:
-                #print("no hand.")
+                # print("no hand.")
                 pass
             # Draw the hand annotations on the image.
             img.flags.writeable = True
@@ -141,28 +188,41 @@ def begin_video_game(width,hight,half_track_width,sample_video,video_id):
                 img,
                 results.pose_landmarks,
                 mp_pose.POSE_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-            img,now_number,sta,game_begin_time,game_end_time = judgment_status(img,right_hand,binarization_arr,x,y,now_number,sta,game_begin_time,game_end_time,half_track_width)
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+            )
+            img, now_number, sta, game_begin_time, game_end_time = judgment_status(
+                img,
+                right_hand,
+                binarization_arr,
+                x,
+                y,
+                now_number,
+                sta,
+                game_begin_time,
+                game_end_time,
+                half_track_width,
+            )
 
-            img=cv2.rectangle(img,(x-10,y-10),(x+10,y+10),(0,0,255),8)   # 畫出手的位置
+            img = cv2.rectangle(
+                img, (x - 10, y - 10), (x + 10, y + 10), (0, 0, 255), 8
+            )  # 畫出手的位置
 
-            cv2.imshow('MediaPipe Pose', img)
+            cv2.imshow("MediaPipe Pose", img)
             if cv2.waitKey(5) & 0xFF == 27:
                 break
-            if sta == 'game_over':
+            if sta == "game_over":
                 time.sleep(5)
                 break
     cap.release()
     cv2.destroyAllWindows()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     sample_video = "test1.mp4"
-    video_id = 0#"test1.mp4" # if need use sample_video "test1.mp4" ,need to delete row:121 -> img=cv2.flip(img, 1) # 畫面翻轉
+    video_id = 0  # "test1.mp4" # if need use sample_video "test1.mp4" ,need to delete row:121 -> img=cv2.flip(img, 1) # 畫面翻轉
     width = 1920
     hight = 1080
-    half_track_width = 50 # 電管'半徑'寬度
-    
-    begin_video_game(width,hight,half_track_width,sample_video,video_id)
+    half_track_width = 50  # 電管'半徑'寬度
+
+    begin_video_game(width, hight, half_track_width, sample_video, video_id)
--- Mazes_from_video/playvideo_and_getcoordinate.py
+++ Mazes_from_video/playvideo_and_getcoordinate.py
@@ -3,6 +3,7 @@ from textwrap import indent
 import cv2
 from cv2 import sqrt
 import mediapipe as mp
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
@@ -10,12 +11,14 @@ import math
 import time
 import numpy as np
 
-def zero_to_one(x): # 使其在0~1間
-    if x > 1.0 :
+
+def zero_to_one(x):  # 使其在0~1間
+    if x > 1.0:
         x = 1.0
-    if x < 0.0 :
+    if x < 0.0:
         x = 0.0
     return x
+
 
 def is_in_poly(point, poly):
     px, py = point
@@ -36,31 +39,55 @@ def is_in_poly(point, poly):
                 is_in = not is_in
     return is_in
 
-def line_length(ax,ay,bx,by):
-    return ((ax-bx)**2)+((ay-by)**2)
 
-def draw_circle(img,coor_arr,nownumber,half_track_width):
+def line_length(ax, ay, bx, by):
+    return ((ax - bx) ** 2) + ((ay - by) ** 2)
+
+
+def draw_circle(img, coor_arr, nownumber, half_track_width):
     for i in range(len(coor_arr)):
         if nownumber == i:
-            col = [255,255,255]
+            col = [255, 255, 255]
         elif nownumber == -1:
-            col = [0,225,225]
-        else :
-            col = [0,255,0]
-        img = cv2.circle(img, (coor_arr[i][0],coor_arr[i][1]), half_track_width, (col[0],col[1],col[2]), -1)
+            col = [0, 225, 225]
+        else:
+            col = [0, 255, 0]
+        img = cv2.circle(
+            img,
+            (coor_arr[i][0], coor_arr[i][1]),
+            half_track_width,
+            (col[0], col[1], col[2]),
+            -1,
+        )
     if nownumber != -1:
-        col = [255,255,255]
-        img = cv2.circle(img, (coor_arr[nownumber][0],coor_arr[nownumber][1]), half_track_width, (col[0],col[1],col[2]), -1)
+        col = [255, 255, 255]
+        img = cv2.circle(
+            img,
+            (coor_arr[nownumber][0], coor_arr[nownumber][1]),
+            half_track_width,
+            (col[0], col[1], col[2]),
+            -1,
+        )
     return img
 
-def draw_dot(img,coor_arr):
+
+def draw_dot(img, coor_arr):
     for i in range(len(coor_arr)):
-        cv2.rectangle(img,(coor_arr[i][0]-1,coor_arr[i][1]-1),(coor_arr[i][0]+1,coor_arr[i][1]+1),(0,0,255),3)   # 畫出觸碰區
+        cv2.rectangle(
+            img,
+            (coor_arr[i][0] - 1, coor_arr[i][1] - 1),
+            (coor_arr[i][0] + 1, coor_arr[i][1] + 1),
+            (0, 0, 255),
+            3,
+        )  # 畫出觸碰區
     return img
 
-def draw_and_save_circle_link_poly(img,coor_a,coor_b,index,binarization_arr,open_save,half_track_width): # get two crycle coordinate and let them link together
-    w = coor_b[0] - coor_a[0]   # x
-    h = coor_b[1] - coor_a[1]   # y
+
+def draw_and_save_circle_link_poly(
+    img, coor_a, coor_b, index, binarization_arr, open_save, half_track_width
+):  # get two crycle coordinate and let them link together
+    w = coor_b[0] - coor_a[0]  # x
+    h = coor_b[1] - coor_a[1]  # y
     dis_len = float((w**2) + (h**2))
     dis_len = math.sqrt(dis_len)
     if dis_len == 0:
@@ -68,124 +95,155 @@ def draw_and_save_circle_link_poly(img,coor_a,coor_b,index,binarization_arr,open
     k = float(float(half_track_width) / float(dis_len))
     link_dot = []
 
-    #right up
-    x = int(coor_a[0] - (k*h))
-    y = int(coor_a[1] + (k*w))
-    link_dot.append([x,y])
+    # right up
+    x = int(coor_a[0] - (k * h))
+    y = int(coor_a[1] + (k * w))
+    link_dot.append([x, y])
 
-    #right down
-    x = int(coor_a[0] + (k*h))
-    y = int(coor_a[1] - (k*w))
-    link_dot.append([x,y])
+    # right down
+    x = int(coor_a[0] + (k * h))
+    y = int(coor_a[1] - (k * w))
+    link_dot.append([x, y])
 
-    #left down
-    x = int(coor_b[0] + (k*h))
-    y = int(coor_b[1] - (k*w))
-    link_dot.append([x,y])
-    
-    #left up
-    x = int(coor_b[0] - (k*h))
-    y = int(coor_b[1] + (k*w))
-    link_dot.append([x,y])
+    # left down
+    x = int(coor_b[0] + (k * h))
+    y = int(coor_b[1] - (k * w))
+    link_dot.append([x, y])
 
-    #draw line
-    cv2.line(img, (link_dot[0][0], link_dot[0][1]), (link_dot[3][0], link_dot[3][1]), (0,0,0), 5)
-    cv2.line(img, (link_dot[1][0], link_dot[1][1]), (link_dot[2][0], link_dot[2][1]), (0,0,0), 5)
-    
-    #full draw
+    # left up
+    x = int(coor_b[0] - (k * h))
+    y = int(coor_b[1] + (k * w))
+    link_dot.append([x, y])
+
+    # draw line
+    cv2.line(
+        img,
+        (link_dot[0][0], link_dot[0][1]),
+        (link_dot[3][0], link_dot[3][1]),
+        (0, 0, 0),
+        5,
+    )
+    cv2.line(
+        img,
+        (link_dot[1][0], link_dot[1][1]),
+        (link_dot[2][0], link_dot[2][1]),
+        (0, 0, 0),
+        5,
+    )
+
+    # full draw
     contours = np.array(link_dot)
-    cv2.fillPoly(img, pts = [contours], color =(0,225,225))
-    
+    cv2.fillPoly(img, pts=[contours], color=(0, 225, 225))
 
-    #save poly (need save ? open_save = 1 : open_save = 0)
+    # save poly (need save ? open_save = 1 : open_save = 0)
     if open_save == 1:
-        ma_x = max(link_dot[0][0],link_dot[1][0],link_dot[2][0],link_dot[3][0])
-        ma_y = max(link_dot[0][1],link_dot[1][1],link_dot[2][1],link_dot[3][1])
-        mi_x = min(link_dot[0][0],link_dot[1][0],link_dot[2][0],link_dot[3][0])
-        mi_y = min(link_dot[0][1],link_dot[1][1],link_dot[2][1],link_dot[3][1])
-        for i in range(mi_x,ma_x+1,1):
-            for j in range(mi_y,ma_y+1,1):
-                if(binarization_arr[index][i][j]==0):
-                    if(is_in_poly([i,j],link_dot)==True):
-                        binarization_arr[index][i][j]=1
-        return img,binarization_arr
-    else :
+        ma_x = max(link_dot[0][0], link_dot[1][0], link_dot[2][0], link_dot[3][0])
+        ma_y = max(link_dot[0][1], link_dot[1][1], link_dot[2][1], link_dot[3][1])
+        mi_x = min(link_dot[0][0], link_dot[1][0], link_dot[2][0], link_dot[3][0])
+        mi_y = min(link_dot[0][1], link_dot[1][1], link_dot[2][1], link_dot[3][1])
+        for i in range(mi_x, ma_x + 1, 1):
+            for j in range(mi_y, ma_y + 1, 1):
+                if binarization_arr[index][i][j] == 0:
+                    if is_in_poly([i, j], link_dot) == True:
+                        binarization_arr[index][i][j] = 1
+        return img, binarization_arr
+    else:
         return img
-      
-def save_crycle_to_3_array(binarization_arr,coor_arr,index,width,hight,half_track_width):
-    binarization_arr.append([[0 for j in range(hight+10)]for i in range(width+10)]) #[x][y]
-    x_begin = coor_arr[index][0]-half_track_width
-    if x_begin < 0 :
+
+
+def save_crycle_to_3_array(
+    binarization_arr, coor_arr, index, width, hight, half_track_width
+):
+    binarization_arr.append(
+        [[0 for j in range(hight + 10)] for i in range(width + 10)]
+    )  # [x][y]
+    x_begin = coor_arr[index][0] - half_track_width
+    if x_begin < 0:
         x_begin = 0
-    x_end =  coor_arr[index][0]+half_track_width
-    if x_end > width :
+    x_end = coor_arr[index][0] + half_track_width
+    if x_end > width:
         x_end = width
-    y_begin = coor_arr[index][1]-half_track_width
-    if y_begin < 0 :
+    y_begin = coor_arr[index][1] - half_track_width
+    if y_begin < 0:
         y_begin = 0
-    y_end =  coor_arr[index][1]+half_track_width
-    if y_end > hight :
+    y_end = coor_arr[index][1] + half_track_width
+    if y_end > hight:
         y_end = hight
-    for i in range(x_begin,x_end):
-        for j in range(y_begin,y_end):
-            if line_length(i,j,coor_arr[index][0],coor_arr[index][1]) < half_track_width**2:
-                binarization_arr[index][i][j] = 1 # is 0-base 
+    for i in range(x_begin, x_end):
+        for j in range(y_begin, y_end):
+            if (
+                line_length(i, j, coor_arr[index][0], coor_arr[index][1])
+                < half_track_width**2
+            ):
+                binarization_arr[index][i][j] = 1  # is 0-base
     return binarization_arr
 
-def get_track(video_name,width,hight,half_track_width):
-    binarization_arr = [] # [nwo_number][hight][width]
+
+def get_track(video_name, width, hight, half_track_width):
+    binarization_arr = []  # [nwo_number][hight][width]
     right_hand = []
     cap = cv2.VideoCapture(str(video_name))
-    #挑整與顯示畫質
+    # 挑整與顯示畫質
     wi = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     hi = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     print("video init Image Size: %d x %d" % (wi, hi))
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width) #設定解析度
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, hight) #設定解析度
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)  # 設定解析度
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, hight)  # 設定解析度
     wi = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     hi = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     print("video fix Image Size: %d x %d" % (wi, hi))
 
-    with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
+    with mp_pose.Pose(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as pose:
         while cap.isOpened():
             success, img = cap.read()
             if not success:
                 print("video end.")
                 for i in range(len(right_hand)):
-                    binarization_arr = save_crycle_to_3_array(binarization_arr,right_hand,i,width,hight,half_track_width)
-                for i in range(1,len(right_hand)):
-                    image,binarization_arr=draw_and_save_circle_link_poly(image,right_hand[i-1],right_hand[i],i,binarization_arr,1,half_track_width)
-                    cv2.imshow('MediaPipe Pose', image)
+                    binarization_arr = save_crycle_to_3_array(
+                        binarization_arr, right_hand, i, width, hight, half_track_width
+                    )
+                for i in range(1, len(right_hand)):
+                    image, binarization_arr = draw_and_save_circle_link_poly(
+                        image,
+                        right_hand[i - 1],
+                        right_hand[i],
+                        i,
+                        binarization_arr,
+                        1,
+                        half_track_width,
+                    )
+                    cv2.imshow("MediaPipe Pose", image)
                     if cv2.waitKey(5) & 0xFF == 27:
                         break
-                #time.sleep(5)
+                # time.sleep(5)
                 cap.release()
                 cv2.destroyAllWindows()
-                return  right_hand , binarization_arr
+                return right_hand, binarization_arr
             else:
                 image = img
-                #image = cv2.resize(img,(width,hight)) # set stand size is 640 * 480.
+                # image = cv2.resize(img,(width,hight)) # set stand size is 640 * 480.
             # To improve performance, optionally mark the image as not writeable to
             # pass by reference.
-            
-            
+
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             results = pose.process(image)
-            size = image.shape   # 取得攝影機影像尺寸
-            w = size[1]        # 取得畫面寬度
-            h = size[0]        # 取得畫面高度
+            size = image.shape  # 取得攝影機影像尺寸
+            w = size[1]  # 取得畫面寬度
+            h = size[0]  # 取得畫面高度
             if results.pose_landmarks:
-                #right hand
+                # right hand
                 rx = zero_to_one(results.pose_landmarks.landmark[20].x)
                 ry = zero_to_one(results.pose_landmarks.landmark[20].y)
-                right_hand.append([int(rx *w),int(ry *h)])
+                right_hand.append([int(rx * w), int(ry * h)])
                 ##left hand
-                #left_hand.append([int(results.pose_landmarks.landmark[19].x *w),int(results.pose_landmarks.landmark[19].y *h)])
+                # left_hand.append([int(results.pose_landmarks.landmark[19].x *w),int(results.pose_landmarks.landmark[19].y *h)])
                 ##right foot
-                #right_foot.append([int(results.pose_landmarks.landmark[32].x *w),int(results.pose_landmarks.landmark[32].y *h)])
+                # right_foot.append([int(results.pose_landmarks.landmark[32].x *w),int(results.pose_landmarks.landmark[32].y *h)])
                 ##left foot
-                #left_foot.append([int(results.pose_landmarks.landmark[31].x *w),int(results.pose_landmarks.landmark[31].y *h)])
+                # left_foot.append([int(results.pose_landmarks.landmark[31].x *w),int(results.pose_landmarks.landmark[31].y *h)])
 
             # Draw the pose annotation on the image.
             image.flags.writeable = True
@@ -194,15 +252,15 @@ def get_track(video_name,width,hight,half_track_width):
                 image,
                 results.pose_landmarks,
                 mp_pose.POSE_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()) 
-            image = draw_circle(img,right_hand,-1,half_track_width)
-            image = draw_dot(image,right_hand)
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+            )
+            image = draw_circle(img, right_hand, -1, half_track_width)
+            image = draw_dot(image, right_hand)
             # Flip the image horizontally for a selfie-view display.
-            #cv2.namedWindow('MediaPipe Pose', 0)
-            #cv2.resizeWindow('MediaPipe Pose', width,hight)
-            cv2.imshow('MediaPipe Pose', image)
+            # cv2.namedWindow('MediaPipe Pose', 0)
+            # cv2.resizeWindow('MediaPipe Pose', width,hight)
+            cv2.imshow("MediaPipe Pose", image)
             if cv2.waitKey(5) & 0xFF == 27:
                 break
     cap.release()
     cv2.destroyAllWindows()
-    
