b7983446cd23454267f27ff245bb19f46514e2dd
asas1asas200 <asas1asas200@gmail.com>
2022-08-06 22:27:10 +0800

    refactor(play): rewrite animations on play page
    Rename demo.js to play.js.
    Rewrite HTML tags of heart and lightning.
        The {{ for n in range(5) }} can be edited to add more hearts in the
        game.
        The flashes of lightning now appear randomly.
    Remove shaking animation on webcam, it can be added back if needed.
    Remove animate.min.css due to unuse.

--- app.py
+++ app.py
@@ -49,11 +49,6 @@ def index():
 @app.route('/play', methods=['POST'])
 def play():
     """Video streaming home page."""
-    dic1 = {"heart" : "changeImage()",
-             "heart1": "changeImage1()",
-             "heart2": "changeImage2()",
-             "heart3": "changeImage3()",
-             "heart4": "changeImage4()"}
     name = r.get('user_name')
     game_mode = request.form["game_mode"]
     game_body = request.form["game_body"]
@@ -64,7 +59,7 @@ def play():
     Camera.processor.sta = 'playing'
     Camera.processor.change_sol(request.form['game_body'])
     Camera.processor.reset()
-    return render_template('play.html', dic1=dic1)
+    return render_template('play.html')
 
 
 @app.route('/gamemode')
--- static/js/HitPoint.js
+++ static/js/HitPoint.js
@@ -1,62 +1,13 @@
-
-// FIXME refactor function
-function changeImage(){
-    element = document.getElementById('heart');
-    if (element.src.match('heart')){
-        element.src ="/static/images/broken-heart.png";
-        const anime = document.querySelector('.lightning');
-        anime.classList.add('animate__animated', 'animate__flash');
-        const shake = document.querySelector('.finalshake');
-        shake.classList.add('animate__animated', 'animate__shakeX')
-        const music = new Audio('static/bgm/Thunder-crack.mp3');
+function heartBroken(element) {
+    if (!element.classList.contains('broken')) {
+        element.src = '/static/images/broken-heart.png';
+        const lightning = document.querySelectorAll('.lightning');
+        var randNum = Math.floor((Math.random() * lightning.length));
+        const target = lightning[randNum];
+        var newone = target.cloneNode(true);
+        target.parentNode.replaceChild(newone, target);
+        const music = new Audio('/static/bgm/Thunder-crack.mp3');
         music.play();
-    }
-}
-
-function changeImage1(){
-    element = document.getElementById('heart1');
-    if (element.src.match('heart')){
-        element.src ="/static/images/broken-heart.png";
-        const anime = document.querySelector('.crack1');
-        anime.classList.add('animate__animated', 'animate__flash');
-        const cam = document.querySelector('.cam');
-        cam.classList.add('animate__animated', 'animate__shakeX')
-        const music = new Audio('static/bgm/Thunder-crack.mp3');
-        music.play();
-    }
-}
-
-function changeImage2(){
-    element = document.getElementById('heart2');
-    if (element.src.match('heart')){
-        element.src ="/static/images/broken-heart.png";
-        const anime = document.querySelector('.crack2');
-        anime.classList.add('animate__animated', 'animate__flash');
-        const photo = document.querySelector('.photo');
-        photo.classList.add('animate__animated', 'animate__shakeX')
-        const music = new Audio('static/bgm/Thunder-crack.mp3');
-        music.play();
-    }
-}
-
-function changeImage3(){
-    element = document.getElementById('heart3');
-    const anime = document.querySelector('.crack3');
-    if (element.src.match('heart')){
-        element.src ="/static/images/broken-heart.png";
-        anime.classList.add('animate__animated','animate__flash');
-        const music = new Audio('static/bgm/Thunder-crack.mp3');
-        music.play();
-    }
-}
-
-function changeImage4(){
-    element = document.getElementById('heart4');
-    const anime = document.querySelector('.crack4');
-    if (element.src.match('heart')){
-        element.src ="/static/images/broken-heart.png";
-        anime.classList.add('animate__animated', 'animate__flash');
-        const music = new Audio('static/bgm/Thunder-crack.mp3');
-        music.play();
+        element.classList.add('broken');
     }
 }
