8590b4a29f20d2cbfc917ffe44b8973994840080
fcebk17 <fcebk17@gmail.com>
2022-08-09 20:23:35 +0800

    change heart img
    As receive "out_pipe" msg
    switch to broken-heart img </3
    and play thunder sound

--- static/js/HitPoint.js
+++ static/js/HitPoint.js
@@ -1,13 +1,25 @@
+var socket = io.connect(window.location.protocol + '//' + document.domain + ':' + location.port);
+var i=0;
+socket.on('out_pipe', function () {
+	console.log("out_pipe", socket.connected)
+    const heartList=['heart0','heart1','heart2','heart3','heart4'];
+    heartBroken(heartList[i]);
+    changeimg(heartList[i]);
+});
+
 function heartBroken(element) {
-    if (!element.classList.contains('broken')) {
-        element.src = '/static/images/broken-heart.png';
-        const lightning = document.querySelectorAll('.lightning');
-        var randNum = Math.floor((Math.random() * lightning.length));
-        const target = lightning[randNum];
-        var newone = target.cloneNode(true);
-        target.parentNode.replaceChild(newone, target);
-        const music = new Audio('/static/bgm/Thunder-crack.mp3');
-        music.play();
-        element.classList.add('broken');
-    }
+    element.src = '/static/images/broken-heart.png';
+    const lightning = document.querySelectorAll('.lightning');
+    var randNum = Math.floor((Math.random() * lightning.length));
+    const target = lightning[randNum];
+    var newone = target.cloneNode(true);
+    target.parentNode.replaceChild(newone, target);
+    const music = new Audio('/static/bgm/Thunder-crack.mp3');
+    music.play();
+}
+
+function changeimg(item) {
+    const hey = document.getElementById(item);
+    hey.src = 'static/images/broken-heart.png';
+    i++;
 }
